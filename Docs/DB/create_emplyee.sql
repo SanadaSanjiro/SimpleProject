89aaaed8896323b3729ed7d89ccf4445f4a91e8f
Alexandr Pogodaev <trollegon@narod.ru>
2023-05-25 17:45:01 +0800

    Filtering added. Homework complete now

---
+++ Docs/DB/create_emplyee.sql
@@ -0,0 +1,12 @@
+create table employees
+(
+	id 				bigserial 	primary key,
+	uid				varchar,
+	firstname		varchar,
+	lastname		varchar,
+	surname			varchar,
+	position		varchar,
+	account			varchar,
+	email			varchar,
+	status			varchar
+);
