d952e696af9863bd5d9871a468e6a8a5ce4ea4e8
Alexandr Pogodaev <trollegon@narod.ru>
2023-06-01 17:24:42 +0800

    My test is running now!

--- src/test/java/com/digdes/simple/testing/service/employee/EmployeeServiceImplTest.java
+++ src/test/java/com/digdes/simple/testing/service/employee/EmployeeServiceImplTest.java
@@ -5,21 +5,30 @@ import com.digdes.simple.dto.employee.EmployeeCrtDTO;
 import com.digdes.simple.dto.employee.EmployeeViewDTO;
 import com.digdes.simple.model.employee.EmployeeModel;
 import com.digdes.simple.model.employee.EmployeeStatus;
+import com.digdes.simple.service.EmployeeService;
+import com.digdes.simple.service.PassEncoder;
 import com.digdes.simple.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class EmployeeServiceImplTest {
-    @Spy
+    @Mock
     EmployeeDAO employeeDAO;
+
+    @Mock
+    PassEncoder pe;
 
     @InjectMocks
     EmployeeServiceImpl employeeService;
@@ -36,15 +45,49 @@ public class EmployeeServiceImplTest {
         dbModel.setFirstName(firstName);
         dbModel.setLastName(lastName);
         dbModel.setStatus(EmployeeStatus.ACTIVE);
-        when(employeeDAO.create(employeeModel)).thenReturn(dbModel);
+        when(employeeDAO.create(any())).thenReturn(dbModel);
         EmployeeCrtDTO dto = new EmployeeCrtDTO();
         dto.setFirstname(firstName);
         dto.setLastname(lastName);
+        System.out.println(dto);
         EmployeeViewDTO result = employeeService.create(dto);
-        verify(employeeDAO, Mockito.times(1)).create(employeeModel);
+        System.out.println(result);
+        verify(employeeDAO, Mockito.times(1)).create(Mockito.any());
         Assertions.assertEquals(firstName, result.getFirstname());
         Assertions.assertEquals(lastName, result.getLastname());
         Assertions.assertEquals(result.getStatus(), "ACTIVE");
         Assertions.assertNotNull(result.getId());
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getFiltered() {
     }
 }
